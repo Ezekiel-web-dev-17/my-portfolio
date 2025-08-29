45707d7c52002b3e7ad1e9fec8cfd75754247a63
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-08-29 11:40:33 +0100

    Implemented constant scroll to top of each page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import RootLayout from "./layout/RootLayout.jsx";
 import Home from "./pages/Home.jsx";
 import AboutMe from "./pages/AboutMe.jsx";
 import { ToastContainer } from "react-toastify";
+import ScrollToTop from "./components/ScrollPageTop.jsx";
 
 function App() {
   return (
     <>
       <ToastContainer />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route element={<RootLayout />}>
             <Route path="/" element={<Home />} />
